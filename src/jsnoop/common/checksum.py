4ca527d849bf68ff72561a17b2aa3542ddcd09b4
Arun Babu Neelicattu <abn@redhat.com>
2013-01-24 15:22:07 +1000

    Supporting seek to previous pos when file-like obj is given for hashing

--- src/jsnoop/common/checksum.py
+++ src/jsnoop/common/checksum.py
@@ -13,13 +13,14 @@ def hexdigest(arg, algorithm='sha512'):
 	try:
 		# Try opening the file or assuming that arg is a file-like object
 		f = open(arg, mode='rb') if isinstance(arg, str) else arg
+		# If we get here, we have to remember where we started
+		last_position = f.tell() if hasattr(f, 'tell') else 0
 		digest = algorithms[algorithm]()
 		for buff in iter(f.read, b''):
 			digest.update(buff)
 		if f is arg:
 			# Assume that we processed a file-like object
-			# TODO: Should we seel to previous position?
-			arg.seek(0)
+			arg.seek(last_position)
 		else:
 			# If we opened a file, be nice and close it
 			f.close()
