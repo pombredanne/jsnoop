80c1fa0b80be09ec4fcce21946ad521379bda926
Arun Babu Neelicattu <abn@redhat.com>
2013-02-26 18:26:26 +1000

    The initial version of the setup script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='jsnoop',
+	version='0.0.0',
+	description='Java Archives and file snooping toolkit.',
+	author='Arun Babu Neelicattu',
+	url='https://github.com/abn/jsnoop',
+
+	install_requires=[],
+)
